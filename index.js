0e75b30a7ccee341f0ebdaac4e74d582d77c20ea
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-06-08 21:27:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeli = [];
 function takeANumber(katzDeli,customer){
   katzDeli.push(customer)
-  return katzDeli.indexOf(customer)
+  var number=katzDeli.indexOf(customer)+1;
+  return ''
 }
