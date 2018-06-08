a3ec779ac962da7739baf23a67535ba880572e0e
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-06-08 22:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ function nowServing(katzDeli){
 }
 function currentLine(katzDeli){
   if (katzDeli.length>0){
-    for (var i=0,i<katzDeli.length,i++){
-      return 
+    for (i=0;i<katzDeli.length;i++){
+      var str="The line is currently: "
+      str+=((i+1)+'. '+katzDeli[i]+',')
     }
+    return str
+  }else{
+    return 
   }
   
 }
