c065f49fc4b43cf2fcfa8fc0fdc3e0a15b57d299
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-06-08 21:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var num
 function takeANumber(katzDeli,customer){
   katzDeli.push(customer);
   num=katzDeli.indexOf(customer);
-  return "Welcome, ${customer}. You are number ${num} in line."
+  return "Welcome,  You are number ${num} in line."
  
 }
