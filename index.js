521e7200d60466632304628947ef98156090689d
oliviatian27@gmail.com <oliviatian27@gmail.com>
2018-06-08 21:32:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeli = [];
+var num
 function takeANumber(katzDeli,customer){
   katzDeli.push(customer);
-  var num=katzDeli.indexOf(customer);
+  num=katzDeli.indexOf(customer);
   return "Welcome, ${customer}. You are number ${num} in line."
 }
